5d1d639266a2187581fc51180873cd7122afa5e6
Enno Gottschalk <mrmaffen@googlemail.com>
2013-01-30 18:30:55 +0100

    Reactivate crash reporting.

--- src/org/tomahawk/tomahawk_android/TomahawkApp.java
+++ src/org/tomahawk/tomahawk_android/TomahawkApp.java
@@ -85,7 +85,7 @@ public class TomahawkApp extends Application implements AccountManagerCallback<B
 
     @Override
     public void onCreate() {
-        //        TomahawkExceptionReporter.init(this);
+        TomahawkExceptionReporter.init(this);
         super.onCreate();
         sApplicationContext = getApplicationContext();
 
