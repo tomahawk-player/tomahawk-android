5cd88dadeb0c0b17af718143e2a2d319a1698e86
Enno Gottschalk <mrmaffen@googlemail.com>
2014-04-11 22:00:33 +0200

    New collection view with FragmentPager.

---
+++ src/org/tomahawk/tomahawk_android/adapters/TomahawkPagerAdapter.java
@@ -0,0 +1,61 @@
+/* == This file is part of Tomahawk Player - <http://tomahawk-player.org> ===
+ *
+ *   Copyright 2014, Enno Gottschalk <mrmaffen@googlemail.com>
+ *
+ *   Tomahawk is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Tomahawk is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Tomahawk. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.tomahawk.tomahawk_android.adapters;
+
+import org.tomahawk.tomahawk_android.TomahawkApp;
+import org.tomahawk.tomahawk_android.fragments.TomahawkFragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.List;
+
+public class TomahawkPagerAdapter extends FragmentPagerAdapter {
+
+    private List<String> mFragmentClassNames;
+
+    private List<String> mFragmentTitles;
+
+    public TomahawkPagerAdapter(FragmentManager fragmentManager, List<String> fragmentClassNames,
+            List<String> fragmentTitles) {
+        super(fragmentManager);
+
+        mFragmentClassNames = fragmentClassNames;
+        mFragmentTitles = fragmentTitles;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        Bundle bundle = new Bundle();
+        bundle.putBoolean(TomahawkFragment.TOMAHAWK_LIST_ITEM_IS_LOCAL, true);
+        return Fragment.instantiate(TomahawkApp.getContext(),
+                mFragmentClassNames.get(position), bundle);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentClassNames.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mFragmentTitles.get(position);
+    }
+}
