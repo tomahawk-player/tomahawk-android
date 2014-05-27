575b7d194b87199b0a414ad5382d7a442df44630
Enno Gottschalk <mrmaffen@googlemail.com>
2014-05-27 22:48:59 +0200

    Added support for native network requests (so we can get them yummy cookies :>)

--- src/org/tomahawk/libtomahawk/resolver/ScriptInterface.java
+++ src/org/tomahawk/libtomahawk/resolver/ScriptInterface.java
@@ -5,12 +5,18 @@ import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.json.JSONObject;
 import org.tomahawk.libtomahawk.infosystem.InfoSystemUtils;
+import org.tomahawk.libtomahawk.utils.TomahawkUtils;
 import org.tomahawk.tomahawk_android.R;
 
+import android.text.TextUtils;
 import android.util.Log;
 import android.webkit.JavascriptInterface;
 
 import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -24,6 +30,26 @@ public class ScriptInterface {
     private ScriptResolver mScriptResolver;
 
     private ObjectMapper mObjectMapper;
+
+    /**
+     * Class to make a callback on the javascript side of this ScriptInterface. The callback is
+     * stored in a map on the js side and can be identified by its callback-id, which is given to
+     * this JsCallback in its constructor.
+     */
+    public class JsCallback {
+
+        private int mCallbackId;
+
+        public JsCallback(int callbackId) {
+            mCallbackId = callbackId;
+        }
+
+        public void call(String responseText, Map<String, List<String>> responseHeaders, int status,
+                String statusText) {
+            mScriptResolver
+                    .callback(mCallbackId, responseText, responseHeaders, status, statusText);
+        }
+    }
 
     ScriptInterface(ScriptResolver scriptResolver) {
         mScriptResolver = scriptResolver;
@@ -49,7 +75,7 @@ public class ScriptInterface {
 
     /**
      * This method is needed because the javascript script is expecting an exposed method which will
-     * return the scriptPath and config. This method is being called in tomahawk_android.js
+     * return the scriptPath and config. This method is being called in tomahawk_android_pre.js
      *
      * @return a {@link JSONObject} containing the scriptPath and config.
      */
@@ -86,7 +112,7 @@ public class ScriptInterface {
     /**
      * This method is needed because the javascript script is expecting an exposed method which it
      * can call to return the resolved {@link Result}s. This method is being called in
-     * tomahawk_android.js
+     * tomahawk_android_pre.js
      *
      * @param in the JSONObject {@link String} containing the resolved {@link Result}s
      */
@@ -97,7 +123,7 @@ public class ScriptInterface {
 
     /**
      * This method is needed because the javascript script is expecting an exposed method which it
-     * can call to report its capabilities. This method is being called in tomahawk_android.js
+     * can call to report its capabilities. This method is being called in tomahawk_android_pre.js
      *
      * @param in the int pointing to the script's capabilities
      */
@@ -107,7 +133,7 @@ public class ScriptInterface {
 
     /**
      * This method is needed because the javascript script is expecting an exposed method which it
-     * can call to report its capabilities. This method is being called in tomahawk_android.js
+     * can call to report its capabilities. This method is being called in tomahawk_android_pre.js
      */
     @JavascriptInterface
     public void addCustomUrlTranslator(String protocol, String callbackFuncName, boolean isAsync) {
@@ -119,9 +145,64 @@ public class ScriptInterface {
     }
 
     @JavascriptInterface
+    public void addCustomUrlHandler(String protocol, String callbackFuncName, boolean isAsync) {
+    }
+
+    @JavascriptInterface
     public String readBase64(String fileName) {
         // We return an empty string because we don't want the base64 string containing png image
         // data or stuff from config.ui.
         return "";
+    }
+
+    @JavascriptInterface
+    public void javaAsyncRequest(final String url, final int callbackId,
+            final String stringifiedExtraHeaders, final String stringifiedOptions,
+            final int errorHandlerId) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Map<String, String> extraHeaders = new HashMap<String, String>();
+                    if (!TextUtils.isEmpty(stringifiedExtraHeaders)) {
+                        extraHeaders = mObjectMapper.readValue(stringifiedExtraHeaders, Map.class);
+                    }
+                    ScriptInterfaceRequestOptions options = null;
+                    if (!TextUtils.isEmpty(stringifiedOptions)) {
+                        options = mObjectMapper.readValue(stringifiedOptions,
+                                ScriptInterfaceRequestOptions.class);
+                    }
+                    JsCallback callback = null;
+                    if (callbackId >= 0) {
+                        callback = new JsCallback(callbackId);
+                    }
+                    String method = null;
+                    String username = null;
+                    String password = null;
+                    String data = null;
+                    JsCallback errorHandler = null;
+                    if (options != null) {
+                        method = options.method;
+                        username = options.username;
+                        password = options.password;
+                        data = options.data;
+                        if (errorHandlerId >= 0) {
+                            errorHandler = new JsCallback(errorHandlerId);
+                        }
+                    }
+                    TomahawkUtils.httpRequest(method, url, extraHeaders, username, password, data,
+                            callback, errorHandler);
+                } catch (NoSuchAlgorithmException e) {
+                    Log.e(TAG,
+                            "javaAsyncRequest: " + e.getClass() + ": " + e.getLocalizedMessage());
+                } catch (KeyManagementException e) {
+                    Log.e(TAG,
+                            "javaAsyncRequest: " + e.getClass() + ": " + e.getLocalizedMessage());
+                } catch (IOException e) {
+                    Log.e(TAG,
+                            "javaAsyncRequest: " + e.getClass() + ": " + e.getLocalizedMessage());
+                }
+            }
+        }).start();
     }
 }
---
+++ src/org/tomahawk/libtomahawk/utils/StringEscapeUtils.java
@@ -0,0 +1,187 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.tomahawk.libtomahawk.utils;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.Locale;
+
+public class StringEscapeUtils {
+
+    /**
+     * <p>Escapes the characters in a <code>String</code> using JavaScript String rules.</p>
+     * <p>Escapes any values it finds into their JavaScript String form. Deals correctly with quotes
+     * and control-chars (tab, backslash, cr, ff, etc.) </p>
+     *
+     * <p>So a tab becomes the characters <code>'\\'</code> and <code>'t'</code>.</p>
+     *
+     * <p>The only difference between Java strings and JavaScript strings is that in JavaScript, a
+     * single quote must be escaped.</p>
+     *
+     * <p>Example:
+     * <pre>
+     * input string: He didn't say, "Stop!"
+     * output string: He didn\'t say, \"Stop!\"
+     * </pre>
+     * </p>
+     *
+     * @param str String to escape values in, may be null
+     * @return String with escaped values, <code>null</code> if null string input
+     */
+    public static String escapeJavaScript(String str) {
+        return escapeJavaStyleString(str, true, true);
+    }
+
+    /**
+     * <p>Escapes the characters in a <code>String</code> using JavaScript String rules to a
+     * <code>Writer</code>.</p>
+     *
+     * <p>A <code>null</code> string input has no effect.</p>
+     *
+     * @param out Writer to write escaped string into
+     * @param str String to escape values in, may be null
+     * @throws IllegalArgumentException if the Writer is <code>null</code>
+     * @throws IOException              if error occurs on underlying Writer
+     * @see #escapeJavaScript(java.lang.String)
+     */
+    public static void escapeJavaScript(Writer out, String str) throws IOException {
+        escapeJavaStyleString(out, str, true, true);
+    }
+
+    /**
+     * <p>Worker method for the {@link #escapeJavaScript(String)} method.</p>
+     *
+     * @param str                String to escape values in, may be null
+     * @param escapeSingleQuotes escapes single quotes if <code>true</code>
+     * @param escapeForwardSlash TODO
+     * @return the escaped string
+     */
+    private static String escapeJavaStyleString(String str, boolean escapeSingleQuotes,
+            boolean escapeForwardSlash) {
+        if (str == null) {
+            return null;
+        }
+        try {
+            StringWriter writer = new StringWriter(str.length() * 2);
+            escapeJavaStyleString(writer, str, escapeSingleQuotes, escapeForwardSlash);
+            return writer.toString();
+        } catch (IOException ioe) {
+            // this should never ever happen while writing to a StringWriter
+            throw new RuntimeException(ioe);
+        }
+    }
+
+    /**
+     * <p>Worker method for the {@link #escapeJavaScript(String)} method.</p>
+     *
+     * @param out                write to receieve the escaped string
+     * @param str                String to escape values in, may be null
+     * @param escapeSingleQuote  escapes single quotes if <code>true</code>
+     * @param escapeForwardSlash TODO
+     * @throws IOException if an IOException occurs
+     */
+    private static void escapeJavaStyleString(Writer out, String str, boolean escapeSingleQuote,
+            boolean escapeForwardSlash) throws IOException {
+        if (out == null) {
+            throw new IllegalArgumentException("The Writer must not be null");
+        }
+        if (str == null) {
+            return;
+        }
+        int sz;
+        sz = str.length();
+        for (int i = 0; i < sz; i++) {
+            char ch = str.charAt(i);
+
+            // handle unicode
+            if (ch > 0xfff) {
+                out.write("\\u" + hex(ch));
+            } else if (ch > 0xff) {
+                out.write("\\u0" + hex(ch));
+            } else if (ch > 0x7f) {
+                out.write("\\u00" + hex(ch));
+            } else if (ch < 32) {
+                switch (ch) {
+                    case '\b':
+                        out.write('\\');
+                        out.write('b');
+                        break;
+                    case '\n':
+                        out.write('\\');
+                        out.write('n');
+                        break;
+                    case '\t':
+                        out.write('\\');
+                        out.write('t');
+                        break;
+                    case '\f':
+                        out.write('\\');
+                        out.write('f');
+                        break;
+                    case '\r':
+                        out.write('\\');
+                        out.write('r');
+                        break;
+                    default:
+                        if (ch > 0xf) {
+                            out.write("\\u00" + hex(ch));
+                        } else {
+                            out.write("\\u000" + hex(ch));
+                        }
+                        break;
+                }
+            } else {
+                switch (ch) {
+                    case '\'':
+                        if (escapeSingleQuote) {
+                            out.write('\\');
+                        }
+                        out.write('\'');
+                        break;
+                    case '"':
+                        out.write('\\');
+                        out.write('"');
+                        break;
+                    case '\\':
+                        out.write('\\');
+                        out.write('\\');
+                        break;
+                    case '/':
+                        if (escapeForwardSlash) {
+                            out.write('\\');
+                        }
+                        out.write('/');
+                        break;
+                    default:
+                        out.write(ch);
+                        break;
+                }
+            }
+        }
+    }
+
+    /**
+     * <p>Returns an upper case hexadecimal <code>String</code> for the given character.</p>
+     *
+     * @param ch The character to convert.
+     * @return An upper case hexadecimal <code>String</code>
+     */
+    private static String hex(char ch) {
+        return Integer.toHexString(ch).toUpperCase(Locale.ENGLISH);
+    }
+}
