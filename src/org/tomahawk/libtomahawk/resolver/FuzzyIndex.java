47ecd594e12947e5458bf07ff660374a7cde04e6
Enno Gottschalk <mrmaffen@googlemail.com>
2014-08-19 17:16:21 +0200

    Added some debug output to FuzzyIndex.

--- src/org/tomahawk/libtomahawk/resolver/FuzzyIndex.java
+++ src/org/tomahawk/libtomahawk/resolver/FuzzyIndex.java
@@ -65,6 +65,7 @@ public class FuzzyIndex {
     public boolean create(String fileName, boolean recreate) {
         synchronized (this) {
             try {
+                Log.d(TAG, "create - fileName:" + fileName + ", recreate:" + recreate);
                 mLucenePath = fileName;
                 File indexDirFile = new File(mLucenePath);
                 Directory dir = FSDirectory.open(indexDirFile);
@@ -80,6 +81,7 @@ public class FuzzyIndex {
     }
 
     public void close() {
+        Log.d(TAG, "close");
         endIndexing();
         try {
             if (mSearcherManager != null) {
@@ -94,6 +96,14 @@ public class FuzzyIndex {
     public void addScriptResolverFuzzyIndexList(ScriptResolverFuzzyIndex[] indexList) {
         synchronized (this) {
             try {
+                Log.d(TAG, "addScriptResolverFuzzyIndexList - count: " + indexList.length);
+                if (indexList.length > 0) {
+                    Log.d(TAG,
+                            "addScriptResolverFuzzyIndexList - first index: id:" + indexList[0].id
+                                    + ", artist:" + indexList[0].artist
+                                    + ", album:" + indexList[0].album
+                                    + ", track:" + indexList[0].track);
+                }
                 beginIndexing(true);
                 for (ScriptResolverFuzzyIndex index : indexList) {
                     Document document = new Document();
@@ -114,6 +124,7 @@ public class FuzzyIndex {
     public void deleteIndex() {
         synchronized (this) {
             try {
+                Log.d(TAG, "deleteIndex");
                 TomahawkUtils.deleteRecursive(new File(mLucenePath));
             } catch (FileNotFoundException e) {
                 Log.e(TAG, "deleteIndex: " + e.getClass() + ": " + e
@@ -138,6 +149,7 @@ public class FuzzyIndex {
                     term = new Term("fulltext", escapedQuery);
                     fqry = new FuzzyQuery(term);
                     qry.add(fqry, BooleanClause.Occur.SHOULD);
+                    Log.d(TAG, "search - fulltext: " + escapedQuery);
                 } else {
                     String escapedTrackName = MultiFieldQueryParser
                             .escape(query.getBasicTrack().getName());
@@ -149,17 +161,23 @@ public class FuzzyIndex {
                     term = new Term("artist", escapedArtistName);
                     fqry = new FuzzyQuery(term);
                     qry.add(fqry, BooleanClause.Occur.MUST);
+                    Log.d(TAG, "search - non-fulltext: " + escapedArtistName + ", "
+                            + escapedTrackName);
                 }
                 IndexSearcher searcher = mSearcherManager.acquire();
                 long time = System.currentTimeMillis();
                 ScoreDoc[] hits = searcher.search(qry, 50).scoreDocs;
-                Log.d(TAG, "searching took " + (System.currentTimeMillis() - time) + "ms");
+                Log.d(TAG, "search - searching took " + (System.currentTimeMillis() - time) + "ms");
                 results = new double[hits.length][2];
                 for (int i = 0; i < hits.length; i++) {
                     ScoreDoc doc = hits[i];
                     Document document = searcher.doc(doc.doc);
                     results[i][0] = document.getField("id").numericValue().intValue();
                     results[i][1] = doc.score;
+                }
+                if (results.length > 0) {
+                    Log.d(TAG, "search - first result: id:" + results[0][0]
+                            + ", score: " + results[0][1]);
                 }
                 mSearcherManager.release(searcher);
             } catch (IOException e) {
@@ -175,6 +193,7 @@ public class FuzzyIndex {
      * @param recreate whether or not to wipe any previously existing index
      */
     private void beginIndexing(boolean recreate) throws IOException {
+        Log.d(TAG, "beginIndexing - recreate: " + recreate);
         endIndexing();
         File indexDirFile = new File(mLucenePath);
         Directory dir = FSDirectory.open(indexDirFile);
@@ -189,6 +208,7 @@ public class FuzzyIndex {
     }
 
     private void endIndexing() {
+        Log.d(TAG, "endIndexing");
         if (mLuceneWriter != null) {
             try {
                 mLuceneWriter.commit();
