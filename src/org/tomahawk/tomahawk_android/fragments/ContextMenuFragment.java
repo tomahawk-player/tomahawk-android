3456ba10ecca20a089ba2df32bb6805ff21e9a48
Enno Gottschalk <mrmaffen@googlemail.com>
2014-09-18 15:48:15 +0200

    Star album, if "add to collection" context menu button clicked.

--- src/org/tomahawk/tomahawk_android/fragments/ContextMenuFragment.java
+++ src/org/tomahawk/tomahawk_android/fragments/ContextMenuFragment.java
@@ -44,6 +44,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -168,6 +169,12 @@ public class ContextMenuFragment extends Fragment {
             addToCollectionButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
+                    if (mTomahawkListItem instanceof Album) {
+                        CollectionManager.getInstance().toggleLovedItem((Album) mTomahawkListItem);
+                    } else {
+                        Toast.makeText(getActivity(), "Not yet implemented for tracks",
+                                Toast.LENGTH_SHORT).show();
+                    }
                     getActivity().getSupportFragmentManager().popBackStack();
                 }
             });
