3a8c6a412f778119925e3589be65459de3fdbfc4
Enno Gottschalk <mrmaffen@googlemail.com>
2014-11-05 00:32:59 +0100

    Allow spotify-metadata.js resolver to handle spotify playlists

--- assets/js/resolvers/spotify-metadata/content/contents/code/spotify-metadata.js
+++ assets/js/resolvers/spotify-metadata/content/contents/code/spotify-metadata.js
@@ -36,52 +36,71 @@ var SpotifyMetadataResolver = Tomahawk.extend(TomahawkResolver, {
     canParseUrl: function (url, type) {
         switch (type) {
         case TomahawkUrlType.Album:
-            return /spotify:album:/.test(url) || /https?:\/\/(play|open)\.spotify\.[^\/]+\/album\//.test(url);
+            return /spotify:album:([^:]+)/.test(url)
+                || /https?:\/\/(?:play|open)\.spotify\.[^\/]+\/album\/([^\/\?]+)/.test(url);
         case TomahawkUrlType.Artist:
-            return /spotify:artist:/.test(url) || /https?:\/\/(play|open)\.spotify\.[^\/]+\/artist\//.test(url);
+            return /spotify:artist:([^:]+)/.test(url)
+                || /https?:\/\/(?:play|open)\.spotify\.[^\/]+\/artist\/([^\/\?]+)/.test(url);
         case TomahawkUrlType.Playlist:
-            return /spotify:user:[^:]*:playlist:/.test(url);
+            return /spotify:user:([^:]+):playlist:([^:]+)/.test(url)
+                || /https?:\/\/(?:play|open)\.spotify\.[^\/]+\/user\/([^\/]+)\/playlist\/([^\/\?]+)/.test(url);;
         case TomahawkUrlType.Track:
-            return /spotify:track:/.test(url) || /https?:\/\/(play|open)\.spotify\.[^\/]+\/track\//.test(url);
+            return /spotify:track:([^:]+)/.test(url)
+                || /https?:\/\/(?:play|open)\.spotify\.[^\/]+\/track\/([^\/\?]+)/.test(url);
         // case TomahawkUrlType.Any:
         default:
-            return /spotify:(album|artist|track|user):/.test(url) || /https?:\/\/(play|open)\.spotify\.[^\/]+\/(album|artist|track)\//.test(url);
+            return /spotify:(album|artist|track):([^:]+)/.test(url)
+                || /https?:\/\/(?:play|open)\.spotify\.[^\/]+\/(album|artist|track)\/([^\/\?]+)/.test(url)
+                || /spotify:user:([^:]+):playlist:([^:]+)/.test(url)
+                || /https?:\/\/(?:play|open)\.spotify\.[^\/]+\/user\/([^\/]+)\/playlist\/([^\/\?]+)/.test(url);
         }
     },
 
     lookupUrl: function (url) {
+        Tomahawk.log("lookupUrl: '" + url + "'");
         var that = this;
-        var match = url.match(/spotify:(album|artist|track):(.*)/);
-        var playlistmatch = url.match(/spotify:user:[^:]*:playlist:(.*)/);
+        var match = url.match(/spotify:(album|artist|track):([^:]+)/);
         if (match == null) {
-            match = url.match(/https?:\/\/(play|open)\.spotify\.[^\/]+\/(album|artist|track)\/([^\/\?]*)/);
-            if (match != null) match.splice(1, 1);
+            match = url.match(/https?:\/\/(?:play|open)\.spotify\.[^\/]+\/(album|artist|track)\/([^\/\?]+)/);
+        }
+        var playlistmatch = url.match(/spotify:user:([^:]+):playlist:([^:]+)/);
+        if (playlistmatch == null) {
+            var playlistmatch = url.match(/https?:\/\/(?:play|open)\.spotify\.[^\/]+\/user\/([^\/]+)\/playlist\/([^\/\?]+)/);
         }
         if (match != null) {
             var query = 'https://ws.spotify.com/lookup/1/.json?uri=spotify:' + match[1] + ':' + match[2];
+            Tomahawk.log("Found album/artist/track, calling " + query);
             Tomahawk.asyncRequest(query, function (xhr) {
                 var res = JSON.parse(xhr.responseText);
                 if (match[1] == "artist") {
-                    Tomahawk.addUrlResult(url, {
+                    var result = {
                         type: "artist",
                         name: res.artist.name
-                    });
+                    };
+                    Tomahawk.addUrlResult(url, result);
+                    Tomahawk.log("Reported found artist '" + result.name + "'");
                 } else if (match[1] == "album") {
-                    Tomahawk.addUrlResult(url, {
+                    var result = {
                         type: "album",
                         name: res.album.name,
-                        artist: res.album.artist,
-                    });
+                        artist: res.album.artist
+                    };
+                    Tomahawk.addUrlResult(url, result);
+                    Tomahawk.log("Reported found album '" + result.name + "' by '" + result.artist + "'");
                 } else if (match[1] == "track") {
-                    Tomahawk.addUrlResult(url, {
+                    var result = {
                         type: "track",
                         title: res.track.name,
                         artist: res.track.artists.map(function (item) { return item.name; }).join(" & ")
-                    });
+                    };
+                    Tomahawk.addUrlResult(url, result);
+                    Tomahawk.log("Reported found track '" + result.title + "' by '" + result.artist + "'");
                 }
             });
         } else if (playlistmatch != null) {
-            var query = 'http://spotikea.tomahawk-player.org/browse/' + url;
+            var query = 'http://spotikea.tomahawk-player.org/browse/spotify:user:' + playlistmatch[1]
+                        + ':playlist:' + playlistmatch[2];
+            Tomahawk.log("Found playlist, calling url: '" + query + "'");
             Tomahawk.asyncRequest(query, function (xhr) {
                 var res = JSON.parse(xhr.responseText);
                 var result = {
@@ -95,6 +114,7 @@ var SpotifyMetadataResolver = Tomahawk.extend(TomahawkResolver, {
                 };
                 result.tracks = res.playlist.result.map(function (item) { return { type: "track", title: item.title, artist: item.artist }; });
                 Tomahawk.addUrlResult(url, result);
+                Tomahawk.log("Reported found playlist '" + result.title + "' containing " + result.tracks.length + " tracks");
             });
         }
     }
