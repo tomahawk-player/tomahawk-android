fd50c26b423155884c38fcd1efc2229f5f1217a9
Enno Gottschalk <mrmaffen@googlemail.com>
2015-05-18 17:42:54 +0200

    Moved Rdio resolver into external plugin app

--- src/org/tomahawk/tomahawk_android/mediaplayers/RdioMediaPlayer.java
+++ src/org/tomahawk/tomahawk_android/mediaplayers/RdioMediaPlayer.java
@@ -17,33 +17,20 @@
  */
 package org.tomahawk.tomahawk_android.mediaplayers;
 
-import com.rdio.android.api.Rdio;
-import com.rdio.android.api.RdioListener;
-
-import org.tomahawk.libtomahawk.authentication.AuthenticatorManager;
-import org.tomahawk.libtomahawk.resolver.PipeLine;
+import org.tomahawk.aidl.IPluginService;
 import org.tomahawk.libtomahawk.resolver.Query;
 import org.tomahawk.libtomahawk.resolver.ScriptJob;
-import org.tomahawk.libtomahawk.resolver.ScriptResolver;
 import org.tomahawk.libtomahawk.resolver.models.ScriptResolverAccessTokenResult;
-import org.tomahawk.libtomahawk.resolver.models.ScriptResolverAppKeysResult;
 import org.tomahawk.tomahawk_android.TomahawkApp;
 
 import android.app.Application;
-import android.media.AudioManager;
-import android.media.MediaPlayer;
+import android.os.RemoteException;
 import android.util.Log;
-
-import java.io.IOException;
-
-import de.greenrobot.event.EventBus;
 
 /**
  * This class wraps a standard {@link android.media.MediaPlayer} object.
  */
-public class RdioMediaPlayer
-        implements TomahawkMediaPlayer, android.media.MediaPlayer.OnPreparedListener,
-        android.media.MediaPlayer.OnErrorListener, android.media.MediaPlayer.OnCompletionListener {
+public class RdioMediaPlayer extends PluginMediaPlayer implements TomahawkMediaPlayer {
 
     private static final String TAG = RdioMediaPlayer.class.getSimpleName();
 
@@ -53,232 +40,38 @@ public class RdioMediaPlayer
 
     }
 
-    private TomahawkMediaPlayerCallback mMediaPlayerCallback;
-
-    private android.media.MediaPlayer mMediaPlayer;
-
-    private Query mPreparedQuery;
-
-    private Query mPreparingQuery;
-
-    private Rdio mRdio;
-
-    private RdioListener mRdioListener = new RdioListener() {
-
-        @Override
-        public void onRdioReadyForPlayback() {
-            Log.d(TAG, "Rdio SDK is ready for playback");
-            prepareQuery(mPreparingQuery);
-        }
-
-        @Override
-        public void onRdioUserPlayingElsewhere() {
-            Log.d(TAG, "onRdioUserPlayingElsewhere()");
-            AuthenticatorManager.ConfigTestResultEvent event
-                    = new AuthenticatorManager.ConfigTestResultEvent();
-            event.mComponent = this;
-            event.mType = AuthenticatorManager.CONFIG_TEST_RESULT_TYPE_PLAYINGELSEWHERE;
-            EventBus.getDefault().post(event);
-            final ScriptResolver rdioResolver = (ScriptResolver) PipeLine.getInstance()
-                    .getResolver(TomahawkApp.PLUGINNAME_RDIO);
-            AuthenticatorManager.showToast(rdioResolver.getPrettyName(), event);
-        }
-
-        /*
-         * Dispatched by the Rdio object once the setTokenAndSecret call has finished, and the credentials are
-         * ready to be used to make API calls.  The token & token secret are passed in so that you can
-         * save/cache them for future re-use.
-         * @see com.rdio.android.api.RdioListener#onRdioAuthorised(java.lang.String, java.lang.String)
-         */
-        @Override
-        public void onRdioAuthorised(String extraToken, String extraTokenSecret) {
-            Log.d(TAG, "Rdio Application authorised.");
-        }
-    };
-
     private RdioMediaPlayer() {
+        super(TomahawkApp.PLUGINNAME_RDIO, "org.tomahawk.rdioplugin");
     }
 
     public static RdioMediaPlayer getInstance() {
         return Holder.instance;
     }
 
-    /**
-     * Start playing the previously prepared {@link org.tomahawk.libtomahawk.collection.Track}
-     */
     @Override
-    public void start() throws IllegalStateException {
-        Log.d(TAG, "start()");
-        if (mMediaPlayer != null) {
-            if (!mMediaPlayer.isPlaying()) {
-                mMediaPlayer.start();
-            }
-            // mMediaplayer.seekTo(0) should be called whenever a Track has just been prepared
-            // and is being started. This workaround is needed because of a bug in Android 4.4.
-            if (mMediaPlayer.getCurrentPosition() == 0) {
-                mMediaPlayer.seekTo(0);
-            }
-        }
-    }
-
-    /**
-     * Pause playing the current {@link org.tomahawk.libtomahawk.collection.Track}
-     */
-    @Override
-    public void pause() throws IllegalStateException {
-        Log.d(TAG, "pause()");
-        if (mMediaPlayer != null) {
-            if (mMediaPlayer.isPlaying()) {
-                mMediaPlayer.pause();
-            }
-        }
-    }
-
-    /**
-     * Seek to the given playback position (in ms)
-     */
-    @Override
-    public void seekTo(int msec) throws IllegalStateException {
-        Log.d(TAG, "seekTo()");
-        // seekTo is currently disabled due to a bug in the rdio SDK
-        // (https://github.com/rdio/api/issues/114)
-    }
-
-    /**
-     * Prepare the given url
-     */
-    @Override
-    public TomahawkMediaPlayer prepare(Application application, Query query,
-            TomahawkMediaPlayerCallback callback) {
-        Log.d(TAG, "prepare()");
-        mMediaPlayerCallback = callback;
-        mPreparedQuery = null;
-        mPreparingQuery = query;
-        release();
-
-        if (mRdio == null || !mRdio.isReady()) {
-            final ScriptResolver rdioResolver = (ScriptResolver) PipeLine.getInstance()
-                    .getResolver(TomahawkApp.PLUGINNAME_RDIO);
-            rdioResolver.getAppKeys(new ScriptJob.ResultsCallback<ScriptResolverAppKeysResult>(
-                    ScriptResolverAppKeysResult.class) {
-                @Override
-                public void onReportResults(final ScriptResolverAppKeysResult appKeysResult) {
-                    rdioResolver.getAccessToken(
-                            new ScriptJob.ResultsCallback<ScriptResolverAccessTokenResult>(
-                                    ScriptResolverAccessTokenResult.class) {
-                                @Override
-                                public void onReportResults(
-                                        ScriptResolverAccessTokenResult accessTokenResult) {
-                                    mRdio = new Rdio(appKeysResult.appKey,
-                                            appKeysResult.appSecret,
-                                            accessTokenResult.accessToken,
-                                            accessTokenResult.accessTokenSecret,
-                                            TomahawkApp.getContext(), mRdioListener);
-                                    mRdio.prepareForPlayback();
+    public ServiceCall getPrepareServiceCall(Application application, final Query query) {
+        return new ServiceCall() {
+            @Override
+            public void call(final IPluginService pluginService) {
+                getScriptResolver().getAccessToken(
+                        new ScriptJob.ResultsCallback<ScriptResolverAccessTokenResult>(
+                                ScriptResolverAccessTokenResult.class) {
+                            @Override
+                            public void onReportResults(
+                                    ScriptResolverAccessTokenResult accessTokenResult) {
+                                String uri = query.getPreferredTrackResult().getPath()
+                                        .replace("rdio://track/", "");
+                                try {
+                                    pluginService.prepare(uri, accessTokenResult.accessToken,
+                                            accessTokenResult.accessTokenSecret, -1);
+                                } catch (RemoteException e) {
+                                    Log.e(TAG, "prepare: " + e.getClass() + ": "
+                                            + e.getLocalizedMessage());
                                 }
-                            });
-                }
-            });
-        } else {
-            prepareQuery(mPreparingQuery);
-        }
-        return this;
-    }
+                            }
+                        });
 
-    private void prepareQuery(Query query) {
-        if (query != null) {
-            try {
-                mMediaPlayer = mRdio.getPlayerForTrack(
-                        mPreparingQuery.getPreferredTrackResult().getPath()
-                                .replace("rdio://track/", ""), null, true);
-            } catch (IllegalStateException e) {
-                Log.e(TAG, "prepare: " + e.getClass() + ": "
-                        + e.getLocalizedMessage());
-                return;
             }
-            if (mMediaPlayer == null) {
-                Log.e(TAG, "prepare: MediaPlayer returned by "
-                        + "Rdio#getPlayerForTrack() was null");
-                return;
-            }
-            mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-            mMediaPlayer.setOnPreparedListener(RdioMediaPlayer.this);
-            mMediaPlayer.setOnErrorListener(RdioMediaPlayer.this);
-            mMediaPlayer.setOnCompletionListener(RdioMediaPlayer.this);
-            try {
-                mMediaPlayer.prepare();
-            } catch (IOException e) {
-                Log.e(TAG, "prepare: " + e.getClass() + ": "
-                        + e.getLocalizedMessage());
-            }
-        }
-    }
-
-    @Override
-    public void release() {
-        Log.d(TAG, "release()");
-        mPreparedQuery = null;
-        if (mMediaPlayer != null) {
-            mMediaPlayer.release();
-            mMediaPlayer = null;
-        }
-    }
-
-    /**
-     * @return the current track position
-     */
-    @Override
-    public int getPosition() {
-        if (mMediaPlayer != null && mPreparedQuery != null) {
-            return mMediaPlayer.getCurrentPosition();
-        } else {
-            return 0;
-        }
-    }
-
-    @Override
-    public boolean isPlaying(Query query) {
-        return mMediaPlayer != null && mPreparedQuery == query && mMediaPlayer.isPlaying();
-    }
-
-    @Override
-    public boolean isPreparing(Query query) {
-        return mPreparingQuery == query;
-    }
-
-    @Override
-    public boolean isPrepared(Query query) {
-        return mPreparedQuery == query;
-    }
-
-    @Override
-    public void onPrepared(android.media.MediaPlayer mp) {
-        Log.d(TAG, "onPrepared()");
-        mPreparedQuery = mPreparingQuery;
-        mPreparingQuery = null;
-        mMediaPlayerCallback.onPrepared(mPreparedQuery);
-    }
-
-    @Override
-    public boolean onError(android.media.MediaPlayer mp, int what, int extra) {
-        Log.d(TAG, "onError()");
-        String whatString = "CODE UNSPECIFIED";
-        switch (what) {
-            case MediaPlayer.MEDIA_ERROR_UNKNOWN:
-                whatString = "MEDIA_ERROR_UNKNOWN";
-                break;
-            case MediaPlayer.MEDIA_ERROR_SERVER_DIED:
-                whatString = "MEDIA_ERROR_SERVER_DIED";
-        }
-        mPreparedQuery = null;
-        mPreparingQuery = null;
-        mMediaPlayerCallback.onError(whatString);
-        return false;
-    }
-
-    @Override
-    public void onCompletion(android.media.MediaPlayer mp) {
-        Log.d(TAG, "onCompletion()");
-        mMediaPlayerCallback.onCompletion(mPreparedQuery);
+        };
     }
 }
