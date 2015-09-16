87028b51d05b117c897c1be4de3b43444ff1eacf
Enno Gottschalk <mrmaffen@googlemail.com>
2015-09-16 02:47:46 +0200

    Improve type handling in Store

---
+++ src/org/tomahawk/libtomahawk/infosystem/hatchet/PlaybackLogEntry.java
@@ -0,0 +1,42 @@
+/* == This file is part of Tomahawk Player - <http://tomahawk-player.org> ===
+ *
+ *   Copyright 2015, Enno Gottschalk <mrmaffen@googlemail.com>
+ *
+ *   Tomahawk is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Tomahawk is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Tomahawk. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.tomahawk.libtomahawk.infosystem.hatchet;
+
+import org.tomahawk.libtomahawk.resolver.Query;
+
+import java.util.Date;
+
+public class PlaybackLogEntry {
+
+    private Query mQuery;
+
+    private Date mTimeStamp;
+
+    public PlaybackLogEntry(Query query, Date timeStamp) {
+        mQuery = query;
+        mTimeStamp = timeStamp;
+    }
+
+    public Query getQuery() {
+        return mQuery;
+    }
+
+    public Date getTimeStamp() {
+        return mTimeStamp;
+    }
+}
