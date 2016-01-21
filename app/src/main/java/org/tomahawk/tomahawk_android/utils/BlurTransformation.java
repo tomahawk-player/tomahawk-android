b3bf1e56a4eb95d117beb54235abbe98a6844928
Enno Gottschalk <mrmaffen@googlemail.com>
2016-01-21 03:14:50 +0100

    Fix memory leak by updating BlurTransformation and CircularImageTransformation

--- app/src/main/java/org/tomahawk/tomahawk_android/utils/BlurTransformation.java
+++ app/src/main/java/org/tomahawk/tomahawk_android/utils/BlurTransformation.java
@@ -1,27 +1,25 @@
-/* == This file is part of Tomahawk Player - <http://tomahawk-player.org> ===
- *
- *   Copyright 2014, Enno Gottschalk <mrmaffen@googlemail.com>
- *
- *   Tomahawk is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
- *
- *   Tomahawk is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with Tomahawk. If not, see <http://www.gnu.org/licenses/>.
- */
 package org.tomahawk.tomahawk_android.utils;
+
+/**
+ * Copyright (C) 2015 Wasabeef
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
 
 import com.squareup.picasso.Transformation;
 
-import org.tomahawk.tomahawk_android.TomahawkApp;
-
+import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.support.v8.renderscript.Allocation;
 import android.support.v8.renderscript.Element;
 import android.support.v8.renderscript.RenderScript;
@@ -29,29 +27,64 @@ import android.support.v8.renderscript.ScriptIntrinsicBlur;
 
 public class BlurTransformation implements Transformation {
 
-    private static final RenderScript mRenderScript = RenderScript.create(TomahawkApp.getContext());
+    private static int MAX_RADIUS = 25;
+
+    private static int DEFAULT_DOWN_SAMPLING = 1;
+
+    private Context mContext;
+
+    private int mRadius;
+
+    private int mSampling;
+
+    public BlurTransformation(Context context) {
+        this(context, MAX_RADIUS, DEFAULT_DOWN_SAMPLING);
+    }
+
+    public BlurTransformation(Context context, int radius) {
+        this(context, radius, DEFAULT_DOWN_SAMPLING);
+    }
+
+    public BlurTransformation(Context context, int radius, int sampling) {
+        mContext = context.getApplicationContext();
+        mRadius = radius;
+        mSampling = sampling;
+    }
 
     @Override
     public Bitmap transform(Bitmap source) {
-        return staticTransform(source, 16f);
+
+        int scaledWidth = source.getWidth() / mSampling;
+        int scaledHeight = source.getHeight() / mSampling;
+
+        Bitmap bitmap = Bitmap.createBitmap(scaledWidth, scaledHeight, Bitmap.Config.ARGB_8888);
+
+        Canvas canvas = new Canvas(bitmap);
+        canvas.scale(1 / (float) mSampling, 1 / (float) mSampling);
+        Paint paint = new Paint();
+        paint.setFlags(Paint.FILTER_BITMAP_FLAG);
+        canvas.drawBitmap(source, 0, 0, paint);
+
+        RenderScript rs = RenderScript.create(mContext);
+        Allocation input = Allocation
+                .createFromBitmap(rs, bitmap, Allocation.MipmapControl.MIPMAP_NONE,
+                        Allocation.USAGE_SCRIPT);
+        Allocation output = Allocation.createTyped(rs, input.getType());
+        ScriptIntrinsicBlur blur = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs));
+
+        blur.setInput(input);
+        blur.setRadius(mRadius);
+        blur.forEach(output);
+        output.copyTo(bitmap);
+
+        source.recycle();
+        rs.destroy();
+
+        return bitmap;
     }
 
     @Override
     public String key() {
-        return "BlurTransformation";
-    }
-
-    public static Bitmap staticTransform(Bitmap source, float radius) {
-        final Allocation input = Allocation.createFromBitmap(mRenderScript, source);
-        // Use this constructor for best performance, because it uses USAGE_SHARED mode which reuses
-        // memory
-        final Allocation output = Allocation.createTyped(mRenderScript, input.getType());
-        final ScriptIntrinsicBlur script =
-                ScriptIntrinsicBlur.create(mRenderScript, Element.U8_4(mRenderScript));
-        script.setRadius(radius);
-        script.setInput(input);
-        script.forEach(output);
-        output.copyTo(source);
-        return source;
+        return "BlurTransformation(radius=" + mRadius + ", sampling=" + mSampling + ")";
     }
 }
