4144292de7050aea69834b80e5442e7dd66efed2
Enno Gottschalk <mrmaffen@googlemail.com>
2016-05-12 21:02:45 +0200

    Always only add root fragments once

--- app/src/main/java/org/tomahawk/tomahawk_android/utils/FragmentUtils.java
+++ app/src/main/java/org/tomahawk/tomahawk_android/utils/FragmentUtils.java
@@ -31,6 +31,7 @@ import org.tomahawk.tomahawk_android.activities.TomahawkMainActivity;
 import org.tomahawk.tomahawk_android.fragments.CollectionPagerFragment;
 import org.tomahawk.tomahawk_android.fragments.ContentHeaderFragment;
 import org.tomahawk.tomahawk_android.fragments.ContextMenuFragment;
+import org.tomahawk.tomahawk_android.fragments.PlaybackFragment;
 import org.tomahawk.tomahawk_android.fragments.SocialActionsFragment;
 import org.tomahawk.tomahawk_android.fragments.TomahawkFragment;
 
@@ -49,72 +50,107 @@ public class FragmentUtils {
 
     public static final String FRAGMENT_TAG = "the_ultimate_tag";
 
+    public static final String ROOT_FRAGMENT_TAG = "root_fragment_tag";
+
+    public static final String PLAYBACK_FRAGMENT_TAG = "playback_fragment_tag";
+
     /**
-     * Add a root fragment as the first fragment the user is seeing after opening the app.
+     * Add a root {@link Fragment} as the first {@link Fragment} the user is seeing after opening
+     * the app.
      *
-     * @param activity     TomahawkMainActivity needed as a context object and to make sure the
-     *                     SlidingLayoutPanel is collapsed
+     * @param activity     {@link TomahawkMainActivity} needed as a context object
      * @param loggedInUser the currently logged-in user object. determines whether to show the feed
      *                     fragment or collection fragment
      */
     public static void addRootFragment(TomahawkMainActivity activity, User loggedInUser) {
-        FragmentTransaction ft = activity.getSupportFragmentManager().beginTransaction();
-        if (!loggedInUser.isOffline()) {
-            Bundle bundle = new Bundle();
-            bundle.putString(TomahawkFragment.USER, loggedInUser.getId());
-            bundle.putInt(TomahawkFragment.SHOW_MODE, SocialActionsFragment.SHOW_MODE_DASHBOARD);
-            bundle.putInt(TomahawkFragment.CONTENT_HEADER_MODE,
-                    ContentHeaderFragment.MODE_ACTIONBAR_FILLED);
-            ft.add(R.id.content_viewer_frame,
-                    Fragment.instantiate(activity, SocialActionsFragment.class.getName(), bundle),
-                    FRAGMENT_TAG);
-            Log.d(TAG,
-                    "Added " + SocialActionsFragment.class.getSimpleName() + " as root fragment.");
-        } else {
-            Bundle bundle = new Bundle();
-            bundle.putString(TomahawkFragment.COLLECTION_ID,
-                    TomahawkApp.PLUGINNAME_USERCOLLECTION);
-            bundle.putInt(TomahawkFragment.CONTENT_HEADER_MODE,
-                    ContentHeaderFragment.MODE_HEADER_STATIC);
-            ft.add(R.id.content_viewer_frame,
-                    Fragment.instantiate(activity, CollectionPagerFragment.class.getName(), bundle),
-                    FRAGMENT_TAG);
-            Log.d(TAG, "Added " + CollectionPagerFragment.class.getSimpleName()
-                    + " as root fragment.");
+        if (activity.getSupportFragmentManager().findFragmentByTag(ROOT_FRAGMENT_TAG) == null) {
+            FragmentTransaction ft = activity.getSupportFragmentManager().beginTransaction();
+            Fragment fragment;
+            if (!loggedInUser.isOffline()) {
+                Bundle bundle = new Bundle();
+                bundle.putString(TomahawkFragment.USER, loggedInUser.getId());
+                bundle.putInt(TomahawkFragment.SHOW_MODE,
+                        SocialActionsFragment.SHOW_MODE_DASHBOARD);
+                bundle.putInt(TomahawkFragment.CONTENT_HEADER_MODE,
+                        ContentHeaderFragment.MODE_ACTIONBAR_FILLED);
+                fragment = Fragment.instantiate(activity, SocialActionsFragment.class.getName(),
+                        bundle);
+                Log.d(TAG, "Added " + SocialActionsFragment.class.getSimpleName()
+                        + " as root fragment.");
+            } else {
+                Bundle bundle = new Bundle();
+                bundle.putString(TomahawkFragment.COLLECTION_ID,
+                        TomahawkApp.PLUGINNAME_USERCOLLECTION);
+                bundle.putInt(TomahawkFragment.CONTENT_HEADER_MODE,
+                        ContentHeaderFragment.MODE_HEADER_STATIC);
+                fragment = Fragment.instantiate(activity, CollectionPagerFragment.class.getName(),
+                        bundle);
+                Log.d(TAG, "Added " + CollectionPagerFragment.class.getSimpleName()
+                        + " as root fragment.");
+            }
+            ft.add(R.id.content_viewer_frame, fragment, ROOT_FRAGMENT_TAG);
+            ft.commitAllowingStateLoss();
         }
-        ft.commitAllowingStateLoss();
     }
 
     /**
-     * Replaces the current {@link Fragment}. This method also automatically collapses the
-     * SlidingPanel.
+     * Add a {@link PlaybackFragment} to the container inside the {@link
+     * com.sothree.slidinguppanel.SlidingUpPanelLayout}
      *
-     * @param activity TomahawkMainActivity needed as a context object and to make sure the
-     *                 SlidingLayoutPanel is collapsed
-     * @param clss     Class of the Fragment to instantiate
-     * @param bundle   Bundle which contains arguments (can be null)
+     * @param activity {@link TomahawkMainActivity} needed as a context object
+     */
+    public static void addPlaybackFragment(TomahawkMainActivity activity) {
+        if (activity.getSupportFragmentManager().findFragmentByTag(PLAYBACK_FRAGMENT_TAG) == null) {
+            Bundle bundle = new Bundle();
+            bundle.putInt(TomahawkFragment.CONTENT_HEADER_MODE,
+                    ContentHeaderFragment.MODE_HEADER_PLAYBACK);
+            replace(activity, PlaybackFragment.class, bundle, R.id.playback_fragment_frame,
+                    PLAYBACK_FRAGMENT_TAG);
+        }
+    }
+
+    /**
+     * Replaces the current {@link Fragment} in the main fragment container.
+     *
+     * @param activity {@link TomahawkMainActivity} needed as a context object
+     * @param clss     Class of the {@link Fragment} to instantiate
+     * @param bundle   {@link Bundle} which contains arguments (can be null)
      */
     public static void replace(TomahawkMainActivity activity, Class clss, Bundle bundle) {
-        replace(activity, clss, bundle, 0);
+        replace(activity, clss, bundle, R.id.content_viewer_frame, FRAGMENT_TAG);
     }
 
     /**
-     * Replaces the current {@link Fragment}. This method also automatically collapses the
-     * SlidingPanel.
+     * Replaces the current {@link Fragment} in the container with the given id.
      *
-     * @param activity   TomahawkMainActivity needed as a context object and to make sure the
-     *                   SlidingLayoutPanel is collapsed
-     * @param clss       Class of the Fragment to instantiate
-     * @param bundle     Bundle which contains arguments (can be null)
-     * @param frameResId the resource id of the ViewGroup in which the Fragment will be replaced
+     * @param activity       {@link TomahawkMainActivity} needed as a context object
+     * @param clss           Class of the {@link Fragment} to instantiate
+     * @param bundle         {@link Bundle} which contains arguments (can be null)
+     * @param containerResId the resource id of the {@link android.view.ViewGroup} in which the
+     *                       Fragment will be replaced
      */
     public static void replace(TomahawkMainActivity activity, Class clss, Bundle bundle,
-            int frameResId) {
+            int containerResId) {
+        replace(activity, clss, bundle, containerResId, FRAGMENT_TAG);
+    }
+
+    /**
+     * Replaces the current {@link Fragment} in the container with the given id. Allows to specify
+     * the {@link String} with which to tag the replaced {@link Fragment}.
+     *
+     * @param activity       {@link TomahawkMainActivity} needed as a context object
+     * @param clss           Class of the {@link Fragment} to instantiate
+     * @param bundle         {@link Bundle} which contains arguments (can be null)
+     * @param containerResId the resource id of the {@link android.view.ViewGroup} in which the
+     *                       Fragment will be replaced
+     * @param tag            a {@link String} id to tag the replaced {@link Fragment} with
+     */
+    public static void replace(TomahawkMainActivity activity, Class clss, Bundle bundle,
+            int containerResId, String tag) {
         FragmentTransaction ft = activity.getSupportFragmentManager().beginTransaction();
-        ft.replace(frameResId == 0 ? R.id.content_viewer_frame : frameResId,
-                Fragment.instantiate(activity, clss.getName(), bundle), FRAGMENT_TAG);
-        if (frameResId == 0) {
-            ft.addToBackStack(FRAGMENT_TAG);
+        ft.replace(containerResId, Fragment.instantiate(activity, clss.getName(), bundle), tag);
+        if (containerResId == R.id.content_viewer_frame) {
+            ft.addToBackStack(tag);
         }
         ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
         ft.commitAllowingStateLoss();
@@ -123,18 +159,19 @@ public class FragmentUtils {
     }
 
     /**
-     * Add the given {@link Fragment}
+     * Add the given {@link Fragment} to the container with the given id.
      *
-     * @param activity   TomahawkMainActivity needed as a context object and to make sure the
-     *                   SlidingLayoutPanel is collapsed
-     * @param clss       Class of the Fragment to instantiate
-     * @param bundle     Bundle which contains arguments (can be null)
-     * @param frameResId the resource id of the ViewGroup in which the Fragment will be replaced
+     * @param activity       {@link TomahawkMainActivity} needed as a context object
+     * @param clss           Class of the {@link Fragment} to instantiate
+     * @param bundle         {@link Bundle} which contains arguments (can be null)
+     * @param containerResId the resource id of the {@link android.view.ViewGroup} to which the
+     *                       Fragment will be added
      */
     public static void add(TomahawkMainActivity activity, Class clss, Bundle bundle,
-            int frameResId) {
+            int containerResId) {
         FragmentTransaction ft = activity.getSupportFragmentManager().beginTransaction();
-        ft.add(frameResId, Fragment.instantiate(activity, clss.getName(), bundle), FRAGMENT_TAG);
+        ft.add(containerResId, Fragment.instantiate(activity, clss.getName(), bundle),
+                FRAGMENT_TAG);
         ft.addToBackStack(FRAGMENT_TAG);
         ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
         ft.commitAllowingStateLoss();
@@ -144,11 +181,11 @@ public class FragmentUtils {
     /**
      * Show the context menu for the given item in the given context.
      *
-     * @param activity     TomahawkMainActivity needed as a context object and to make sure the
-     *                     SlidingLayoutPanel is collapsed
-     * @param item         The TomahawkListItem for which to show the context menu
-     * @param collectionId the id of the corresponding collection (this is being used to e.g. get an
-     *                     album's tracklist from a specific collection)
+     * @param activity     {@link TomahawkMainActivity} needed as a context object
+     * @param item         The item for which to show the context menu
+     * @param collectionId the id of the corresponding {@link org.tomahawk.libtomahawk.collection.Collection}
+     *                     (this is being used to e.g. get an album's track list from a specific
+     *                     collection)
      */
     public static boolean showContextMenu(TomahawkMainActivity activity, Object item,
             String collectionId, boolean isFromPlaybackFragment, boolean hideRemoveButton) {
